96fb23d71bc465bd6c663c28f011cd45d3d14cf6
Marcos Longo <mlongo@fas.harvard.edu>
2009-11-06 12:01:39 -0500

    Minor change in ed_history_io.F90, the safe limits were not as safe as they should be.

--- ED/src/io/ed_history_io.F90
+++ ED/src/io/ed_history_io.F90
@@ -12,6 +12,7 @@ subroutine read_ed1_history_file
   use grid_coms,only:ngrids
   use allometry, only: dbh2h,h2dbh,dbh2bd,dbh2bl, ed_biomass,area_indices
   use fuse_fiss_utils, only: sort_cohorts
+  use disturb_coms , only : min_new_patch_area ! ! intent(in)
   implicit none
 
   integer :: year
@@ -56,10 +57,10 @@ subroutine read_ed1_history_file
   real(kind=8), dimension(max_water) :: dwater
   
   ! This is the smallest representable number in single precision
-  real(kind=8), parameter :: snglmin=dble(tiny(1.))
-  real, parameter :: min_area=epsilon(1.) ! Doesn't need to be the machine epsilon, chose just a small number.
-  real(kind=8), parameter :: min_ok=dble(tiny(1.)/epsilon(1.)) ! Chose a number small enough, but with some
-                                                               ! room for multiplying by a small area.
+  real(kind=8), parameter :: min_area = 1.d-7     ! Doesn't need to be the machine epsilon,
+                                                  !     just chose a small number.
+  real(kind=8), parameter :: min_ok   = 1.d-20    ! Chose a number small enough, but with some
+                                                  !     room for multiplying by a small area.
   ! Cohort variables from the restart
 
   integer :: ierr
@@ -98,6 +99,8 @@ subroutine read_ed1_history_file
   real                  , dimension(maxfiles)           :: plon_list,plat_list
   real                  , dimension(maxfiles)           :: clon_list,clat_list
   real                  , dimension(maxfiles)           :: file_pdist,file_cdist
+  !----- External function. ---------------------------------------------------------------!
+  real                  , external                      :: sngloff
   !----------------------------------------------------------------------------------------!
   
   
@@ -215,17 +218,17 @@ subroutine read_ed1_history_file
                       dstsl,dssc,dpsc,dmsn,dfsn,dwater(1:nwater)
                  if(ierr /= 0)exit count_patches
              
-                 area(ip)   = sngl(max(snglmin,darea  ))
-                 age(ip)    = sngl(max(min_ok ,dage   ))
-                 fsc(ip)    = sngl(max(min_ok ,dfsc   ))
-                 stsc(ip)   = sngl(max(min_ok ,dstsc  ))
-                 stsl(ip)   = sngl(max(min_ok ,dstsl  ))
-                 ssc(ip)    = sngl(max(min_ok ,dssc   ))
-                 psc(ip)    = sngl(max(min_ok ,dpsc   ))
-                 msn(ip)    = sngl(max(min_ok ,dmsn   ))
-                 fsn(ip)    = sngl(max(min_ok ,dfsn   ))
+                 area(ip)   = sngloff(darea      ,min_area)
+                 age(ip)    = sngloff(dage       ,min_ok  )
+                 fsc(ip)    = sngloff(dfsc       ,min_ok  )
+                 stsc(ip)   = sngloff(dstsc      ,min_ok  )
+                 stsl(ip)   = sngloff(dstsl      ,min_ok  )
+                 ssc(ip)    = sngloff(dssc       ,min_ok  )
+                 psc(ip)    = sngloff(dpsc       ,min_ok  )
+                 msn(ip)    = sngloff(dmsn       ,min_ok  )
+                 fsn(ip)    = sngloff(dfsn       ,min_ok  )
                  do nw=1,nwater
-                    water(nw,ip)  = sngl(max(min_ok ,dwater(nw) ))
+                    water(nw,ip) = sngloff(dwater(nw) ,min_ok  )
                  end do
                  
               case(2)  !! read ED2 format files
@@ -233,15 +236,15 @@ subroutine read_ed1_history_file
                       ,dstsl,dssc,dummy,dmsn,dfsn
                  if(ierr /= 0)exit count_patches
               
-                 area(ip)   = sngl(max(snglmin,darea  ))
-                 age(ip)    = sngl(max(min_ok ,dage   ))
-                 fsc(ip)    = sngl(max(min_ok ,dfsc   ))
-                 stsc(ip)   = sngl(max(min_ok ,dstsc  ))
-                 stsl(ip)   = sngl(max(min_ok ,dstsl  ))
-                 ssc(ip)    = sngl(max(min_ok ,dssc   ))
-                 msn(ip)    = sngl(max(min_ok ,dmsn   ))
-                 fsn(ip)    = sngl(max(min_ok ,dfsn   ))
-                 water(1,ip)  = sngl(max(min_ok ,dwater(1) ))
+                 area(ip)    = sngloff(darea    ,min_area)
+                 age(ip)     = sngloff(dage     ,min_ok  )
+                 fsc(ip)     = sngloff(dfsc     ,min_ok  )
+                 stsc(ip)    = sngloff(dstsc    ,min_ok  )
+                 stsl(ip)    = sngloff(dstsl    ,min_ok  )
+                 ssc(ip)     = sngloff(dssc     ,min_ok  )
+                 msn(ip)     = sngloff(dmsn     ,min_ok  )
+                 fsn(ip)     = sngloff(dfsn     ,min_ok  )
+                 water(1,ip) = sngloff(dwater(1),min_ok  )
                  
               case(3)
                  
@@ -249,7 +252,7 @@ subroutine read_ed1_history_file
                       darea,water(1,ip),fsc(ip),stsc(ip),stsl(ip),ssc(ip),psc(ip),msn(ip),fsn(ip)
                  if(ierr /= 0)exit count_patches
               
-                 area(ip)=sngl(max(snglmin,darea))
+                 area(ip)=sngloff(darea, min_area)
                  
                  if(sitenum(ip)<= 0) continue !! check for valid site number
                  
@@ -561,8 +564,8 @@ subroutine read_ed1_history_file
                        call area_indices(cpatch%nplant(ic2),cpatch%bleaf(ic2)              &
                                         ,cpatch%bdead(ic2),cpatch%balive(ic2)              &
                                         ,cpatch%dbh(ic2), cpatch%hite(ic2)                 &
-                                        ,cpatch%pft(ic2), SLA(cpatch%pft(ic2)), cpatch%lai(ic2) &
-                                        ,cpatch%wpa(ic2), cpatch%wai(ic2))
+                                        ,cpatch%pft(ic2), SLA(cpatch%pft(ic2))             &
+                                        ,cpatch%lai(ic2),cpatch%wpa(ic2), cpatch%wai(ic2))
                        
                        cpatch%cb(1:12,ic2) = cb(1:12,ic)
                        cpatch%cb_max(1:12,ic2) = cb_max(1:12,ic)
@@ -675,6 +678,7 @@ subroutine read_ed1_history_file
            enddo
            cpoly%patch_count(isi) = nsitepat
         enddo
+        
         
      end do polyloop
 
