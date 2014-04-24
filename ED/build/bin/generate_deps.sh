ea5cd3ecb645d5d8d70e92b28a55ce45fd9b7319
Ryan Knox <rgknox@lbl.gov>
2014-04-24 16:56:45 -0700

    Reverted paths.mk to master, ie ED_ROOT = ../.. Reverted generate_deps.sh to master, uses -i~
    It is possible that odd'essy brand generate_deps.sh is better because it is silent,
    but I wanted to stay consistent and use the building tools from the master
    because it worked out of the box.

--- ED/build/bin/generate_deps.sh
+++ ED/build/bin/generate_deps.sh
@@ -3,9 +3,9 @@ edroot=${1}
 includes="-I ${edroot}/src/include"
 edsrc="${edroot}/src/*/*.f90 ${edroot}/src/*/*.F90 ${edroot}/src/*/*.c"
 rm -f dependency.mk
-./sfmakedepend.pl ${includes} -f dependency.mk ${bramssrc} ${edsrc} 2> /dev/null
-sed -i s@hdf5.mod@@g dependency.mk
-sed -i s@leaf.coms.mod@@g dependency.mk
-sed -i s@grid_dims.mod@@g dependency.mk
-sed -i s@rconstants.mod@@g dependency.mk
+./sfmakedepend.pl ${includes} -f dependency.mk ${bramssrc} ${edsrc}
+sed -i~ s@hdf5.mod@@g dependency.mk
+sed -i~ s@leaf.coms.mod@@g dependency.mk
+sed -i~ s@grid_dims.mod@@g dependency.mk
+sed -i~ s@rconstants.mod@@g dependency.mk
 /bin/rm -f dependency.alt dependency.mk.old*
