a5b7122eaf2644d0ab2ec63af3c6144caa5c98fd
crollinson <crollinson@gmail.com>
2015-02-17 09:56:01 -0500

    Through fall etc to temporary extra snow layer

--- ED/src/dynamics/rk4_derivs.F90
+++ ED/src/dynamics/rk4_derivs.F90
@@ -553,11 +553,11 @@ subroutine leaftw_derivs(mzg,mzs,initp,dinitp,csite,ipa,dt,is_hybrid)
    ! temporary "snow" layer.                                                               !
    !---------------------------------------------------------------------------------------!
    if ( ksn > 0 ) then
-      dinitp%sfcwater_mass  (ksn) =  dewgnd +  wshed_tot +  throughfall_tot -  wflxsc
-      dinitp%sfcwater_energy(ksn) = dinitp%sfcwater_energy(ksn)                            &
-                                  + qdewgnd + qwshed_tot + qthroughfall_tot - qwflxsc      &
-                                  - hflxsc  + dble(csite%rlong_s(ipa))
-      dinitp%sfcwater_depth (ksn) = ddewgnd + dwshed_tot + dthroughfall_tot
+      dinitp%sfcwater_mass  (ksn+1) =  dewgnd +  wshed_tot +  throughfall_tot -  wflxsc
+      dinitp%sfcwater_energy(ksn+1) = dinitp%sfcwater_energy(ksn)                            &
+                                  + qdewgnd + qwshed_tot + qthroughfall_tot - qwflxsc      !&
+                                  !- hflxsc  + dble(csite%rlong_s(ipa))
+      dinitp%sfcwater_depth (ksn+1) = ddewgnd + dwshed_tot + dthroughfall_tot
    else
       dinitp%virtual_water        =  dewgnd +  wshed_tot +  throughfall_tot -  wflxsc
       dinitp%virtual_energy       = qdewgnd + qwshed_tot + qthroughfall_tot - qwflxsc
