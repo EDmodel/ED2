3dc26a3bd46495f79212ea2ba25cd222f7895e03
Marcos Longo <mdplongo@gmail.com>
2015-04-06 18:33:31 -0400

    1. Removed the extra rshort_s from h_flux_s and replaced mzg by ksn, following discussions    with Ryan Knox and Christy Rollinson.

--- ED/src/dynamics/rk4_derivs.F90
+++ ED/src/dynamics/rk4_derivs.F90
@@ -516,8 +516,8 @@ subroutine leaftw_derivs(mzg,mzs,initp,dinitp,csite,ipa,dt,is_hybrid)
    rk4aux(ibuff)%h_flux_g(mzg+1) = rk4aux(ibuff)%h_flux_g(mzg+1)                           &
                                  + dinitp%avg_sensible_gg (mzg)
    !---------------------------------------------------------------------------------------!
-   rk4aux(ibuff)%h_flux_s(mzs+1) = rk4aux(ibuff)%h_flux_s(mzs+1) + hflxsc + qwflxsc        &
-                                 - dble(csite%rlong_s(ipa)) - dble(csite%rshort_s(mzs,ipa))
+   rk4aux(ibuff)%h_flux_s(ksn+1) = rk4aux(ibuff)%h_flux_s(ksn+1) + hflxsc + qwflxsc        &
+                                 - dble(csite%rlong_s(ipa))
 
 
 
